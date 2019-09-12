2200b64ed22fabaf86d5f75e3fccb5f63b44ea75
outofmyelement <mackenzie.cooper99@gmail.com>
2019-09-11 21:20:14 -0700

    Trying to clean application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import './footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      HOLLOLOLOLO
       <footer className ="Footer">
-
+WOLOLOLOLOLO
       </footer>
       </div>
     );
