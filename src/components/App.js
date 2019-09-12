97c4af7a28b66db15c61beffd1b3dd243a666a6a
outofmyelement <mackenzie.cooper99@gmail.com>
2019-09-11 21:42:47 -0700

    cleaned up repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../styles/App.css';
 import './footer.js'
 
 class App extends Component {
@@ -7,10 +7,11 @@ class App extends Component {
     return (
       <div className="App">
       HOLLOLOLOLO
-      <footer className ="Footer">
-WOLOLOLOLOLO
-      </footer>
+      <footer className ="footer">
+        WOLOLOLOLOLO
+        </footer>
       </div>
+
     );
       
   }
