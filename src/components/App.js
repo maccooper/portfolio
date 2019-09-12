2cb0e3de232818a5c383c9082042700892c82d2b
Outofmyelement <Mackenzie.Cooper99gmail.com>
2019-09-12 15:06:00 -0700

    added images

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import '../styles/App.css';
 import './footer.js'
+import gitimg from '../assets/github_cat.png'
+import linkedinimg from '../assets/linkedin.png'
+import twitterbird from '../assets/twitter_bird.png'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       HOLLOLOLOLO
-              <footer className ="footer">
-        WOLOLOLOLOLO
+              <footer className="footer">
+                <img src={gitimg} alt='' className={'gitcatt'}></img>
+                <img src={linkedinimg} alt='' className={'gitcatt'}></img>
+                <img src={twitterbird} alt='' className={'gitcatt'}></img>
+                <img src={gitimg} alt='' className={'gitcatt'}></img>
         </footer>
+      
       </div>
 
     );
       
   }
 }
+
+const Base = () => (
+  <div>
+    <div> what the heck</div>
+  </div>
+)
+
+const rootDiv = document.getElementById('root');
+
+ReactDOM.render(<Base/>, rootDiv);
 
 export default App;
  
