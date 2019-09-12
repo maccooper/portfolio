2177b268e5111724b8e1b444b5f48961adbed0a9
Mackcooper <Mackenzie.Cooper99@gmail.com>
2019-09-11 18:49:05 -0700

    Cleared default clutter

---
+++ source/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      <footer className ="Footer">
+
+      </footer>
+      </div>
+    );
+      
+  }
+}
+
+export default App;
+ 
---
+++ source/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '.Footer.css';
+
+const Footer = () => {
+    return (
+        <div classname="App">
+        MY FOOTER
+        </div>
+    )
+}
+
+export default Footer;
