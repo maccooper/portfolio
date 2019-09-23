acd0fcbf82543d20ed79ea5326fb2101eb9b6914
outofmyelement <mackenzie.cooper99@gmail.com>
2019-09-23 15:05:59 -0700

    added image styles + button

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 
 import '../styles/App.css';
 import Footer from './Footer.js'
+import Description from './Description.js'
+import profpic from '../assets/profpic.png'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Footer/>
-      </div>
+      <div>
+        <div className="App">
+          <img src={profpic} alt='' className='profpic'></img>
+          <button className='button'></button>
+        </div>      
+        <div className="Description">
+          <Description className='Description'/>
+        </div>
+        <div className='footer'>
+          <Footer />
+        </div>
 
+      </div>
     );
-      
+
   }
 }
 
 
 export default App;
- 
---
+++ src/components/Description.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Description.css'
+
+
+const Description = () => {
+    return(
+        <div className='Description'>
+            <h1> This is a personal description</h1>
+        </div>
+
+    )
+}
+
+export default Description;
