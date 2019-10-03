13d658b4e007c75eee2a85d00164d83d97ec0952
outofmyelement <mackenzie.cooper99@gmail.com>
2019-10-03 15:27:57 -0700

    removed envolope error and added header component

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,15 @@ import '../styles/App.css';
 import Footer from './Footer.js'
 import Description from './Description.js'
 import Wrapper from './Wrapper.js'
-
+import Header from './Header.js'
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
+        
         <Wrapper/>
 
         <Description/>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Header.css'
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <h1> test </h1>
+        </div>
+    )
+}
+
+
+export default Header;
